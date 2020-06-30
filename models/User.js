4500b5bb073114d1613c67a573814e2f7db72273
danlopes-arc <danlpsarcanjo@gmail.com>
2020-06-29 21:30:38 -0400

    Create User model

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  credit: {
+    type: Number,
+    default: 0
+  },
+  role:{
+    type: String,
+    enum: ['user', 'manager', 'admin'],
+    required: true
+  }
+})
+
+const User = mongoose.model('user', UserSchema)
+
+module.exports = User
