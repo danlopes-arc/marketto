c6d59f0607c8789e523738fb4df2d583efa9753b
danlopes-arc <danlpsarcanjo@gmail.com>
2020-06-29 21:27:12 -0400

    Setup express and MongoDB connection

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log('[mongo] connected'))
+  .catch(err => console.error(err))
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json({ success: true })
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`[server] listening to http://localhost:${port}`)
+})
